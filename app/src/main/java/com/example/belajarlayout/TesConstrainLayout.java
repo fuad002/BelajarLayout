1f10118f517d163b470c360ac2182ab8e650e9d1
petik <fuadhidayat2019.@gmail.com>
2021-02-04 09:16:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarlayout/TesConstrainLayout.java
@@ -0,0 +1,13 @@
+package com.example.belajarlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TesConstrainLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tes_constrain_layout);
+    }
+}
---
+++ app/src/main/java/com/example/belajarlayout/TesRelativeLayout.java
@@ -0,0 +1,14 @@
+package com.example.belajarlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TesRelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tes_relative_layout);
+    }
+}
